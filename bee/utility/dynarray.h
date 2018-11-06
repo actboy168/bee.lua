d38bccdf82a62250b441f014272cceefb3b88a1b
actboy168 <actboy168@gmail.com>
2018-11-06 21:40:12 +0800

    array_view

--- bee/utility/dynarray.h
+++ bee/utility/dynarray.h
@@ -1,31 +1,21 @@
 #pragma once
 
-#include <cassert>
-#include <exception>
-#include <limits>
-#include <iterator>
+#include <bee/utility/array_view.h>
 
 namespace std {
-
-	class bad_array_length : public std::bad_alloc
-	{
+	class bad_array_length : public bad_alloc {
 	public:
-		bad_array_length() throw()
-		{ }
-
-		virtual ~bad_array_length() throw()
-		{ }
-
-		virtual const char* what() const throw()
-		{ 
+		bad_array_length() throw() { }
+		virtual ~bad_array_length() throw() { }
+		virtual const char* what() const throw() { 
 			return "std::bad_array_length"; 
 		}
 	};
 
 	template <class T>
-	class dynarray
-	{
+	class dynarray : public array_view<T> {
 	public:
+		typedef       array_view<T>                   mybase;
 		typedef       T                               value_type;
 		typedef       T&                              reference;
 		typedef const T&                              const_reference;
@@ -38,121 +28,99 @@ namespace std {
 		typedef size_t                                size_type;
 		typedef ptrdiff_t                             difference_type;
 
-		dynarray();
-		const dynarray operator=(const dynarray&);
-
 		explicit dynarray(size_type c)
-			: store_(alloc(c))
-			, count_(c)
+			: mybase(alloc(c), c)
 		{ 
 			size_type i = 0;
 			try {
-				for (i = 0; i < count_; ++i)
-				{
-					new (store_+i) T;
+				for (i = 0; i < mybase::count_; ++i) {
+					new (mybase::store_+i) T;
 				}
 			}
 			catch (...) {
 				for (; i > 0; --i)
-					(store_+(i-1))->~T();
+					(mybase::store_+(i-1))->~T();
 				throw;
 			} 
 		}
 
 		dynarray(const dynarray& d)
-			: store_(alloc(d.size()))
-			, count_(d.size())
+			: mybase(alloc(d.size()), d.size())
 		{ 
 			try { 
-				uninitialized_copy(d.begin(), d.end(), begin());
+				uninitialized_copy(d.begin(), d.end(), mybase::begin());
 			}
 			catch (...) {
-				delete[] reinterpret_cast<char*>(store_);
+				delete[] reinterpret_cast<char*>(mybase::store_);
 				throw; 
 			} 
 		}
 
 		dynarray(dynarray&& d)
-			: store_(d.store_)
-			, count_(d.count_)
+			: mybase(d.store_, d.count_)
 		{
 			d.store_ = nullptr;
 			d.count_ = 0;
 		}
 
 		dynarray(std::initializer_list<T> l)
-			: store_(alloc(l.size()))
-			, count_(l.size())
+			: mybase(alloc(l.size()), l.size())
 		{
 			try {
-				uninitialized_copy(l.begin(), l.end(), begin());
+				uninitialized_copy(l.begin(), l.end(), mybase::begin());
 			}
 			catch (...) {
-				delete[] reinterpret_cast<char*>(store_);
+				delete[] reinterpret_cast<char*>(mybase::store_);
 				throw;
 			}
 		}
 
 		template <class Vec>
 		dynarray(const Vec& v, typename std::enable_if<std::is_same<typename Vec::value_type, T>::value>::type* =0)
-			: store_(alloc(v.size()))
-			, count_(v.size())
+			: mybase(alloc(v.size()), v.size())
 		{
 			try {
-				uninitialized_copy(v.begin(), v.end(), begin());
+				uninitialized_copy(v.begin(), v.end(), mybase::begin());
 			}
 			catch (...) {
-				delete[] reinterpret_cast<char*>(store_);
+				delete[] reinterpret_cast<char*>(mybase::store_);
 				throw;
 			}
 		}
 
 		~dynarray()
 		{
-			for (size_type i = 0; i < size(); ++i)
-			{
-				(store_+i)->~T();
+			for (size_type i = 0; i < mybase::count_; ++i) {
+				(mybase::store_+i)->~T();
 			}
-			delete[] reinterpret_cast<char*>(store_);
+			delete[] reinterpret_cast<char*>(mybase::store_);
 		}
 
-		iterator               begin()                       { return store_; }
-		const_iterator         begin()                 const { return store_; }
-		const_iterator         cbegin()                const { return store_; }
-		iterator               end()                         { return begin() + size(); }
-		const_iterator         end()                   const { return cbegin() + size(); }
-		const_iterator         cend()                  const { return cbegin() + size(); }
-		reverse_iterator       rbegin()                      { return reverse_iterator(end()); }
-		const_reverse_iterator rbegin()                const { return reverse_iterator(cend()); }
-		reverse_iterator       rend()                        { return reverse_iterator(begin()); }
-		const_reverse_iterator rend()                  const { return reverse_iterator(cbegin()); }
-		size_type              size()                  const { return count_; }
-		size_type              max_size()              const { return count_; }
-		bool                   empty()                 const { return size() == 0; }
-		reference              operator[](size_type n)       { assert(size() > n); return store_[n]; }
-		const_reference        operator[](size_type n) const { assert(size() > n); return store_[n]; }
-		reference              front()                       { assert(size() > 0); return store_[0]; }
-		const_reference        front()                 const { assert(size() > 0); return store_[0]; }
-		reference              back()                        { assert(size() > 0); return store_[size()-1]; }
-		const_reference        back()                  const { assert(size() > 0); return store_[size()-1]; }
-		const_reference        at(size_type n)         const { check(n); return store_[n]; }
-		reference              at(size_type n)               { check(n); return store_[n]; }
-		pointer                data()                        { return store_; }
-		const_pointer          data()                  const { return store_; }
-		void                   fill(const T& v)              { fill_n(begin(), size(), v); }
+		dynarray& operator=(const dynarray& d) {
+			if (this != &d) {
+				mybase::store_ = alloc(d.count_);
+				mybase::count_ = d.count_;
+				try {
+					uninitialized_copy(d.begin(), d.end(), mybase::begin());
+				}
+				catch (...) {
+					delete[] reinterpret_cast<char*>(mybase::store_);
+					throw;
+				}
+			}
+			return *this;
+		}
+		dynarray& operator=(dynarray&& d) {
+			if (this != &d) {
+				mybase::store_ = d.store_;
+				mybase::count_ = d.count_;
+				d.store_ = nullptr;
+				d.count_ = 0;
+			}
+			return *this;
+		}
 
 	private:
-		pointer   store_;
-		size_type count_;
-
-		void check(size_type n)
-		{ 
-			if (n >= size())
-			{
-				throw out_of_range("std::dynarray"); 
-			}
-		}
-
 		pointer alloc(size_type n)
 		{ 
 			if (n > (std::numeric_limits<size_type>::max)()/sizeof(T))
