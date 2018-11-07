f2300abfc29292d6f6f38ab2e82625656026deb7
最萌小汐 <sumneko@hotmail.com>
2018-11-07 21:38:09 +0800

    更新测试

--- test/test_subprocess.lua
+++ test/test_subprocess.lua
@@ -1,1 +1,13 @@
 local subprocess = require 'bee.subprocess'
+local fs = require 'bee.filesystem'
+local exe = fs.path(arg[-3])
+
+local function wait_second()
+    local f = io.popen('ping -n 1 127.1>nul', 'r')
+    f:read 'a'
+    f:close()
+end
+
+-- spawn
+local process = subprocess.spawn { exe:string() }
+assert(process ~= nil)
