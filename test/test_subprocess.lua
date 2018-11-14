3eb66c5397d851e128fff61cf5e5b43de742b9bf
最萌小汐 <sumneko@hotmail.com>
2018-11-14 18:07:35 +0800

    更新测试

--- test/test_subprocess.lua
+++ test/test_subprocess.lua
@@ -8,6 +8,62 @@ local function wait_second()
     f:close()
 end
 
--- spawn
-local process = subprocess.spawn { exe }
-assert(process ~= nil)
+-- subprocess.spawn
+local lua = subprocess.spawn { exe }
+assert(lua ~= nil)
+
+fs.remove(fs.path 'temp')
+local lua = subprocess.spawn {
+    exe,
+    '-e', 'io.open("temp", "w"):close()'
+}
+assert(lua ~= nil)
+wait_second()
+assert(fs.exists(fs.path 'temp') == true)
+fs.remove(fs.path 'temp')
+
+-- wait
+fs.remove(fs.path 'temp')
+local lua = subprocess.spawn {
+    exe,
+    '-e', 'io.open("temp", "w"):close()'
+}
+assert(lua ~= nil)
+assert(fs.exists(fs.path 'temp') ~= true)
+lua:wait()
+assert(fs.exists(fs.path 'temp') == true)
+fs.remove(fs.path 'temp')
+
+-- is_running
+local lua = subprocess.spawn { exe }
+assert(lua ~= nil)
+assert(lua:is_running() == true)
+
+-- kill
+local lua = subprocess.spawn { exe }
+assert(lua ~= nil)
+assert(lua:is_running() == true)
+assert(lua:kill() == true)
+assert(lua:is_running() == false)
+assert(lua:kill() == false)
+
+-- get_id
+local lua = subprocess.spawn { exe }
+assert(lua ~= nil)
+local id = lua:get_id()
+local f = io.popen(('tasklist /FI "PID eq %d"'):format(id), 'r')
+local buf = f:read 'a'
+f:close()
+assert(buf:find(exe:filename():string(), 1, true) ~= nil)
+
+--
+local lua, stdin, stdout, stderr = subprocess.spawn {
+    exe,
+    stdin = true,
+    stdout = true,
+    stderr = true,
+}
+assert(lua ~= nil)
+assert(stdin ~= nil)
+assert(stdout ~= nil)
+assert(stderr ~= nil)
