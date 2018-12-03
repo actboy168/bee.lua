decfa5b334b3bdbfb59d821d2197c016da67cf69
actboy168 <actboy168@gmail.com>
2018-12-03 21:21:02 +0800

    导出

--- bee/error.h
+++ bee/error.h
@@ -1,11 +1,12 @@
 #pragma once
 
+#include <bee/config.h>
 #include <system_error>
 
 namespace bee {
-    int last_syserror();
-    int last_neterror();
-    std::system_error make_syserror(const char* message = nullptr);
-    std::system_error make_neterror(const char* message = nullptr);
-    std::system_error make_error(int err, const char* message = nullptr);
+    _BEE_API int last_syserror();
+	_BEE_API int last_neterror();
+	_BEE_API std::system_error make_syserror(const char* message = nullptr);
+	_BEE_API std::system_error make_neterror(const char* message = nullptr);
+	_BEE_API std::system_error make_error(int err, const char* message = nullptr);
 }
