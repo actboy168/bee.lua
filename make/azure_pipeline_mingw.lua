d2e47ac34ca88fcc69275ae736bab72844c1e3ea
actboy168 <actboy168@gmail.com>
2018-12-09 05:16:31 +0800

    修正计算依赖的bug

--- make/azure_pipeline_mingw.lua
+++ make/azure_pipeline_mingw.lua
@@ -1,9 +1,6 @@
-local path, cmd = ...
+local path = ...
 local msys2 = require "msys2"
 msys2:initialize(path)
-if cmd then
-    msys2:exec(cmd)
-end
 msys2:exec("gcc --version")
 msys2:exec("./configure")
 msys2:exec("make")
--- make/makefile_deps.lua
+++ make/makefile_deps.lua
@@ -24,28 +24,16 @@ local function writefile(path, str)
 end
 
 local res = {}
+local alldeps = {}
 
 local function cpp_to_o(path)
     return path:gsub("\\", "_"):gsub("%.cpp", "")
 end
 
-local function gen_cpp(path, deps, sysdeps)
-    path = path:string()
-    res[#res + 1] = ("$(TMPDIR)/%s.o : %s %s | $(TMPDIR)"):format(cpp_to_o(path), path, table.concat(deps, " "))
-    local inc = ""
-    if sysdeps["lua.hpp"] then
-        inc = inc .. " -I$(LUADIR)"
-    end
-    if sysdeps["lua-seri.h"] then
-        inc = inc .. " -I$(LUASERIDIR)"
-    end
-    res[#res + 1] = ("\t$(CXX) -c $(CFLAGS) -o $@ $< %s -I."):format(inc)
-    res[#res + 1] = ""
-end
-
-local function gen_dir(dir, deps)
+local function deps_dir(dir)
     foreach_dir(dir, function(path)
-        if path:extension():string() ~= ".cpp" then
+        local ext = path:extension():string()
+        if ext ~= ".cpp" and ext ~= ".h" then
             return
         end
         local source = readfile(path)
@@ -58,13 +46,66 @@ local function gen_dir(dir, deps)
                 sysdeps[name] = true
             end
         end
-        gen_cpp(path, deps, sysdeps)
+        alldeps[path:string():gsub("\\", "/")] = {deps, sysdeps}
+    end)
+end
+
+local function calc_deps_file(p, a, b)
+    if a[p] then
+        return
+    end
+    a[#a+1] = p
+    a[p] = true
+    for _, v in ipairs(alldeps[p][1]) do
+        calc_deps_file(v, a, b)
+    end
+    for v in pairs(alldeps[p][2]) do
+        b[v] = true
+    end
+end
+
+local function calc_deps()
+    local new = {}
+    for p, d in pairs(alldeps) do
+        local a = {}
+        local b = {}
+        calc_deps_file(p, a, b)
+        new[p] = {a, b}
+    end
+    alldeps = new
+end
+
+local function gen_file(path)
+    path = path:string()
+    local deps, sysdeps = alldeps[path:gsub("\\", "/")][1], alldeps[path:gsub("\\", "/")][2]
+    res[#res + 1] = ("$(TMPDIR)/%s.o : %s %s | $(TMPDIR)"):format(cpp_to_o(path), path, table.concat(deps, " "))
+    local inc = ""
+    if sysdeps["lua.hpp"] then
+        inc = inc .. " -I$(LUADIR)"
+    end
+    if sysdeps["lua-seri.h"] then
+        inc = inc .. " -I$(LUASERIDIR)"
+    end
+    res[#res + 1] = ("\t$(CXX) -c $(CFLAGS) -o $@ $< %s -I."):format(inc)
+    res[#res + 1] = ""
+end
+
+local function gen_dir(dir)
+    foreach_dir(dir, function(path)
+        if path:extension():string() ~= ".cpp" then
+            return
+        end
+        gen_file(path)
     end)
 end
 
 res[#res + 1] = "$(TMPDIR)/lua-seri.o : $(LUASERIDIR)/lua-seri.c $(LUASERIDIR)/lua-seri.h | $(TMPDIR)"
 res[#res + 1] = "\t$(CC) -c $(CFLAGS) -o $@ $< -I$(LUADIR) "
 res[#res + 1] = ""
+
+deps_dir(fs.path "bee")
+deps_dir(fs.path "binding")
+calc_deps()
 
 gen_dir(fs.path "bee")
 gen_dir(fs.path "binding")
