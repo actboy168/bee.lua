5bd81284da7580f52a24c391461b93f0dbcd5c2d
actboy168 <actboy168@gmail.com>
2018-12-09 03:24:57 +0800

    mingw的构建脚本

---
+++ make/azure_pipeline_mingw.lua
@@ -0,0 +1,5 @@
+local path = ...
+local msys2 = require "msys2"
+msys2:initialize(path)
+msys2:exec("gcc --version")
+msys2:exec("./configure; make")
---
+++ make/luamake/msys2.lua
@@ -0,0 +1,24 @@
+local fs = require 'bee.filesystem'
+
+local function convertpath(s)
+    return s:gsub("\\", "/"):gsub("([a-zA-z]):", "/%1")
+end
+
+local mt = {}
+
+function mt:initialize(path)
+    self.path = fs.path(path)
+    self.bash = self.path / "usr" / "bin" / "bash.exe"
+end
+
+function mt:exec(cmd, cwd)
+    cwd = cwd or fs.current_path():string()
+    local command = ([[%s -lc "cd %s; %s"]]):format(self.bash:string(), convertpath(cwd), convertpath(cmd))
+    local f = io.popen(command, 'r')
+    for l in f:lines() do
+        print(l)
+    end
+    f:close()
+end
+
+return mt
