cd418ce98c1c5df650c0d4a1b3bfd7f68c54abb1
actboy168 <actboy168@gmail.com>
2018-12-09 04:05:10 +0800

    加个参数

--- make/azure_pipeline_mingw.lua
+++ make/azure_pipeline_mingw.lua
@@ -1,5 +1,9 @@
-local path = ...
+local path, cmd = ...
 local msys2 = require "msys2"
 msys2:initialize(path)
+if cmd then
+    msys2:exec(cmd)
+end
 msys2:exec("gcc --version")
-msys2:exec("./configure; make")
+msys2:exec("./configure")
+msys2:exec("make")
