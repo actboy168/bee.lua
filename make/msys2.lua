60a3b2b4c81b49de7e04b6d298f88195b06e3926
actboy168 <actboy168@gmail.com>
2018-12-09 14:24:08 +0800

    使用subprocee处理异常的情况

--- make/msys2.lua
+++ make/msys2.lua
@@ -1,4 +1,5 @@
 local fs = require 'bee.filesystem'
+local sp = require 'bee.subprocess'
 
 local function convertpath(s)
     return s:gsub("\\", "/"):gsub("([a-zA-z]):", "/%1")
@@ -13,16 +14,29 @@ end
 
 function mt:exec(cmd, cwd)
     cwd = cwd or fs.current_path():string()
-    local command = ([[%s -lc "export PATH=$PATH:/mingw64/bin:/usr/local/bin:/usr/bin:/bin:/opt/bin; cd %s; %s"]]):format(
-        self.bash:string(),
+    local command = ([[export PATH=$PATH:/mingw64/bin:/usr/local/bin:/usr/bin:/bin:/opt/bin; cd %s; %s]]):format(
         convertpath(cwd),
         convertpath(cmd)
     )
-    local f = io.popen(command, 'r')
-    for l in f:lines() do
-        print(l)
+    local procees, stdout, stderr = sp.spawn {
+        self.bash,
+        "-lc",
+        command,
+        stderr = true,
+        stdout = true,
+    }
+    for l in stdout:lines() do
+        io.stdout:write(l .. '\n')
     end
-    f:close()
+    stdout:close()
+    for l in stderr:lines() do
+        io.stderr:write(l .. '\n')
+    end
+    stderr:close()
+    local code = procees:wait()
+    if code ~= 0 then
+        os.exit(code, true)
+    end
 end
 
 return mt
