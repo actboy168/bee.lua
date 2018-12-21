25bed6d526d7bbf30c4408f4c224639181abcf18
actboy168 <actboy168@gmail.com>
2018-12-21 11:23:53 +0800

    增加一个测试

--- project/make/lua.mk
+++ project/make/lua.mk
@@ -8,6 +8,8 @@ alllua : $(BINDIR)/bee
 LUALDFLAGS = -lreadline
 endif
 
+LUA_FLAGS += -DLUAI_MAXCCALLS=200
+
 CORE_O=	lapi.o lcode.o lctype.o ldebug.o ldo.o ldump.o lfunc.o lgc.o llex.o lmem.o lobject.o lopcodes.o lparser.o lstate.o lstring.o ltable.o ltm.o lundump.o lvm.o lzio.o
 LIB_O=	lauxlib.o lbaselib.o lcorolib.o ldblib.o liolib.o lmathlib.o loadlib.o loslib.o lstrlib.o ltablib.o lutf8lib.o linit.o
 LUA_O=	lua.o
--- test/test_lua.lua
+++ test/test_lua.lua
@@ -2,11 +2,21 @@ local lu = require 'luaunit'
 
 test_lua = {}
 
-function test_lua:test_stack_overflow()
+function test_lua:test_stack_overflow_1()
     lu.assertError(function()
         local function a()
             a()
         end
         a()
+    end)
+end
+
+
+function test_lua:test_stack_overflow_2()
+    lu.assertError(function()
+        local t = setmetatable({}, {__index = function(t)
+            local y = table.concat(t,'',1,1)
+        end})
+        print(t[1])
     end)
 end
