cab36df025d8a27c025ae1e49c2389c6cb4197d2
actboy168 <actboy168@gmail.com>
2019-01-04 21:10:35 +0800

    修正一个bug

--- script/bee.lua
+++ script/bee.lua
@@ -38,7 +38,7 @@ if platform.CRT == 'mingw' then
     end
 
     function path_mt.parent_path(path)
-        return fs.path(path:string():match("(.+)[/\\][%w_.-]*$"))
+        return fs.path(path:string():match("(.+)[/\\][%w_.-]*$") or "")
     end
 end
 
