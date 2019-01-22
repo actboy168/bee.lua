17d4082e88061c6c61bea60858cb253ca815dd2b
actboy168 <actboy168@gmail.com>
2019-01-22 17:28:53 +0800

    算错了

--- make/msvc-init.lua
+++ make/msvc-init.lua
@@ -2,7 +2,7 @@ local helper = require 'msvc_helper'
 local prefix = helper.get_prefix(helper.get_env(helper.get_path()))
 local fs = require 'bee.filesystem'
 
-local cwd = fs.exe_path():parent_path()
+local cwd = fs.exe_path():parent_path():parent_path()
 local outdir = cwd / 'build' / 'msvc'
 local output = outdir / 'msvc_deps_prefix.ninja'
 fs.create_directories(outdir)
