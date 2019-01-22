88a2e42a22908e67462af7cd358a25d4d5fb6fd8
actboy168 <actboy168@gmail.com>
2019-01-22 17:23:51 +0800

    计算当前路径

--- make/msvc-init.lua
+++ make/msvc-init.lua
@@ -1,8 +1,12 @@
 local helper = require 'msvc_helper'
 local prefix = helper.get_prefix(helper.get_env(helper.get_path()))
+local fs = require 'bee.filesystem'
+
+local cwd = fs.exe_path():parent_path()
+local output = cwd / 'build' / 'msvc' / 'msvc_deps_prefix.ninja'
 
 assert(
     assert(
-        io.open('build/msvc/msvc_deps_prefix.ninja', 'wb')
+        io.open(output:string(), 'wb')
     ):write(("deps_prefix = %s"):format(prefix))
 ):close()
