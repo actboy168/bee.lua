031cc93ce4dad4491f5067584e67b7bee429417b
actboy168 <actboy168@gmail.com>
2019-01-22 11:06:03 +0800

    macos的编译

--- project/luamake/macos.lua
+++ project/luamake/macos.lua
@@ -9,3 +9,55 @@ lm:executable 'lua' {
     defines = "LUA_USE_MACOSX",
     links = { "m", "dl" },
 }
+
+lm.rootdir = ''
+
+lm:shared_library 'bee' {
+    includes = {
+        "3rd/lua/src",
+        "3rd/lua-seri",
+        "3rd/incbin",
+        "."
+    },
+    sources = {
+        "3rd/lua-seri/*.c",
+        "bee/*.cpp",
+        "binding/*.cpp",
+        "!bee/*_win.cpp",
+        "!bee/*_linux.cpp",
+        "!bee/utility/path_helper.cpp",
+        "!binding/lua_unicode.cpp",
+        "!binding/lua_filesystem.cpp",
+        "!binding/lua_registry.cpp",
+    },
+    ldflags = {
+        "-framework CoreFoundation",
+        "-framework CoreServices",
+    }
+}
+
+lm:executable 'bootstrap' {
+    includes = {
+        "3rd/lua/src"
+    },
+    sources = {
+        "3rd/lua/src/*.c",
+        "!3rd/lua/src/lua.c",
+        "!3rd/lua/src/luac.c",
+        "bootstrap/*.cpp",
+    },
+    defines = {
+        "LUA_USE_MACOSX",
+        "LUAI_MAXCCALLS=200"
+    },
+    links = { "m", "dl" },
+}
+
+lm:build "copy_script" {
+    "cp", "bootstrap/main.lua", "$bin/main.lua"
+}
+
+lm:build "test" {
+    "$bin/bootstrap", "test/test.lua",
+    deps = { "bootstrap", "copy_script", "bee" },
+}
