458b6a5736f908bfc9e2bd5210e33dd65fbebd64
actboy168 <actboy168@gmail.com>
2019-01-22 13:27:46 +0800

    no rebuilt

--- update.bat
+++ update.bat
@@ -1,5 +1,5 @@
 chcp 65001
-..\luamake\luamake init -p msvc
-..\luamake\luamake init -p mingw
-..\luamake\luamake init -p linux
-..\luamake\luamake init -p macos
+..\luamake\luamake init -rebuilt no -p msvc
+..\luamake\luamake init -rebuilt no -p mingw
+..\luamake\luamake init -rebuilt no -p linux
+..\luamake\luamake init -rebuilt no -p macos
