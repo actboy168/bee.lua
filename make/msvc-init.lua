f2f833936259c7209571a0cdc6e9c7655d655873
actboy168 <actboy168@gmail.com>
2019-01-22 17:27:26 +0800

    需要有目录

--- make/msvc-init.lua
+++ make/msvc-init.lua
@@ -3,7 +3,9 @@ local prefix = helper.get_prefix(helper.get_env(helper.get_path()))
 local fs = require 'bee.filesystem'
 
 local cwd = fs.exe_path():parent_path()
-local output = cwd / 'build' / 'msvc' / 'msvc_deps_prefix.ninja'
+local outdir = cwd / 'build' / 'msvc'
+local output = outdir / 'msvc_deps_prefix.ninja'
+fs.create_directories(outdir)
 
 assert(
     assert(
