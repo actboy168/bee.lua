bd680ed25db1b71c6fe2d9aea40893e7ef54181d
actboy168 <actboy168@gmail.com>
2019-01-24 21:01:42 +0800

    加个测试

--- test/test_lua.lua
+++ test/test_lua.lua
@@ -11,7 +11,6 @@ function test_lua:test_stack_overflow_1()
     end)
 end
 
-
 function test_lua:test_stack_overflow_2()
     lu.assertError(function()
         local t = setmetatable({}, {__index = function(t)
@@ -19,4 +18,22 @@ function test_lua:test_stack_overflow_2()
         end})
         print(t[1])
     end)
+end
+
+function test_lua:test_next()
+    local t = {}
+    for i = 1, 26 do
+        t[string.char(0x40+i)] = true
+    end
+    local expected = {
+        'Z', 'Y', 'V', 'U', 'X', 'W', 'R', 'Q', 'T', 'S', 'N', 'M', 'P', 'O', 'J', 'I', 'L', 'K', 'F', 'E', 'H', 'G', 'B', 'A', 'D', 'C'
+    }
+    local function checkOK()
+        local key
+        for i = 1, 26 do
+            key = next(t, key)
+            lu.assertEquals(key, expected[i])
+        end
+    end
+    checkOK()
 end
