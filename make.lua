bcbf253e2c64e291f82c9058e07617e124de8f55
actboy168 <actboy168@gmail.com>
2019-05-09 21:47:28 +0800

    没用了

--- make.lua
+++ make.lua
@@ -1,23 +1,5 @@
 local lm = require 'luamake'
 
-local outdir = lm.plat
-if lm.plat == "msvc" then
-    local arguments = {}
-    local arg = table.pack(...)
-    local i = 1
-    while i <= #arg do
-        if arg[i]:sub(1, 1) == '-' then
-            local k = arg[i]:sub(2)
-            i = i + 1
-            arguments[k] = arg[i]
-        end
-        i = i + 1
-    end
-    local arch = arguments.arch or 'x86'
-    outdir = outdir .. '_' .. arch
-end
-outdir = outdir .. '_release'
-
 lm.c = lm.plat == 'msvc' and 'c89' or 'c11'
 lm.cxx = 'c++17'
 
