0b52a7c7304b1036caa2d5b7510abd538de5b633
actboy168 <actboy168@gmail.com>
2019-09-29 11:57:51 +0800

    支持xcode11

--- bee/filesystem.h
+++ bee/filesystem.h
@@ -1,6 +1,18 @@
 #pragma once
 
-#if __has_include(<filesystem>) && !defined(__MINGW32__)
+#if __has_include(<filesystem>)
+#   if defined(__MINGW32__)
+#   elif defined(__ENVIRONMENT_MAC_OS_X_VERSION_MIN_REQUIRED__)
+#       if __ENVIRONMENT_MAC_OS_X_VERSION_MIN_REQUIRED__ >= 101500
+#           define BEE_ENABLE_FILESYSTEM 1
+#       endif
+#   else
+#       define BEE_ENABLE_FILESYSTEM 1
+#   endif
+#endif
+
+#if defined(BEE_ENABLE_FILESYSTEM)
+#undef BEE_ENABLE_FILESYSTEM
 #include <filesystem>
 namespace fs = std::filesystem;
 #else
