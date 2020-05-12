e64e189c93d4fe17b3203d6ed57a1591ee2cada9
actboy168 <actboy168@gmail.com>
2020-05-12 11:10:20 +0800

    mingw10支持span

--- bee/span.h
+++ bee/span.h
@@ -2,13 +2,21 @@
 
 #if defined(_MSC_VER)
 #   include <yvals_core.h>
+#   if __has_include(<span>) && _HAS_CXX20
+#       define BEE_ENABLE_SPAN 1
+#   endif
+#elif defined(__MINGW32__)
+#   if __has_include(<span>) && __cplusplus > 201703L
+#       define BEE_ENABLE_SPAN 1
+#   endif
 #endif
 
-#if defined(_MSC_VER) && _HAS_CXX20 && __has_include(<span>)
-#	include <span>
+
+#if defined(BEE_ENABLE_SPAN)
+#   include <span>
 #else
 #   include <stdexcept>
-#	include <bee/nonstd/span.h>
+#   include <bee/nonstd/span.h>
 namespace std {
     using nonstd::span;
 }
