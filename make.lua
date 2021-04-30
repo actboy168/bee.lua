8715676e8b322641174c4c8365e356d493a0050d
actboy168 <actboy168@gmail.com>
2021-04-30 23:44:39 +0800

    更新luamake支持

--- make.lua
+++ make.lua
@@ -9,9 +9,10 @@ local isWindows = lm.plat == 'mingw' or lm.plat == 'msvc'
 local exe = isWindows and ".exe" or ""
 local dll = isWindows and ".dll" or ".so"
 
-lm:build "copy_script" {
-    "{COPY}", "@bootstrap/main.lua", "@$bin/main.lua",
-    deps = { "bootstrap" },
+lm:copy "copy_script" {
+    input = "bootstrap/main.lua",
+    output = "$bin/main.lua",
+    deps = "bootstrap",
 }
 
 lm:build "test" {
