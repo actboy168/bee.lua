6a19a69fb0d4e5443c511ac1ccb8c8de33f1b130
actboy168 <actboy168@gmail.com>
2021-08-09 22:29:45 +0800

    增加android构建脚本

---
+++ project/android.lua
@@ -0,0 +1,72 @@
+local lm = require 'luamake'
+
+lm:source_set 'source_lua' {
+    sources = {
+        "3rd/lua/*.c",
+        "!3rd/lua/luac.c",
+        "!3rd/lua/lua.c",
+        "!3rd/lua/utf8_*.c",
+    },
+    defines = {
+        "LUA_USE_LINUX",
+    },
+    visibility = "default",
+}
+
+lm:executable 'lua' {
+    deps = "source_lua",
+    sources = {
+        "3rd/lua/lua.c",
+    },
+    ldflags = "-Wl,-E",
+    defines = {
+        "LUA_USE_LINUX",
+    },
+    links = {
+        "m", "dl",
+        -- https://gcc.gnu.org/bugzilla/show_bug.cgi?id=67791
+        "pthread",
+    }
+}
+
+lm:shared_library 'bee' {
+    includes = {
+        "3rd/lua",
+        "3rd/lua-seri",
+        "bee/nonstd",
+        "."
+    },
+    sources = {
+        "3rd/lua-seri/*.c",
+        "bee/*.cpp",
+        "bee/nonstd/fmt/*.cc",
+        "binding/*.cpp",
+        "!bee/*_win.cpp",
+        "!bee/*_osx.cpp",
+        "!binding/lua_unicode.cpp",
+        "!binding/lua_registry.cpp",
+        "!binding/lua_wmi.cpp",
+    },
+    links = {
+        "pthread",
+    }
+}
+
+lm:executable 'bootstrap' {
+    deps = "source_lua",
+    includes = {
+        "3rd/lua"
+    },
+    sources = {
+        "bootstrap/*.cpp",
+    },
+    ldflags = "-Wl,-E",
+    defines = {
+        "LUA_USE_LINUX",
+    },
+    links = {
+        "m", "dl",
+        -- https://gcc.gnu.org/bugzilla/show_bug.cgi?id=67791
+        "pthread",
+    }
+}
