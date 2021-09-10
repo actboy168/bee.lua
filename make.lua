1d2dab26c6d843da9532bd7c6ad45275a158258a
actboy168 <actboy168@gmail.com>
2021-09-10 16:44:41 +0800

    没必要检查

--- make.lua
+++ make.lua
@@ -5,13 +5,7 @@ if lm.EXE == "lua" then
 end
 
 if lm.universal then
-    local function IsArm64Macos()
-        local f <close> = assert(io.popen("uname -v", "r"))
-        if f:read "l":match "RELEASE_ARM64" then
-            return true
-        end
-    end
-    if lm.os == "macos" and IsArm64Macos() then
+    if lm.os == "macos" then
         lm:import 'project/macos-universal.lua'
         return
     end
