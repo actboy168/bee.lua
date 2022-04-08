e5014862e9328453fa0e28e3842bec28d9a40cfe
actboy168 <actboy168@gmail.com>
2022-04-08 20:23:42 +0800

    cleanup

--- bee/subprocess.h
+++ bee/subprocess.h
@@ -3,11 +3,7 @@
 #include <bee/config.h>
 
 #if defined(_WIN32)
-#   if defined(_MSC_VER)
 #   include <bee/subprocess/subprocess_win.h>
 #else
-#   include <bee/subprocess/subprocess_win.h>
-#endif
-#else
-#include <bee/subprocess/subprocess_posix.h>
+#   include <bee/subprocess/subprocess_posix.h>
 #endif
