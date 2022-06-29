3a596d731873bd2da171251e973d5033f4046307
actboy168 <actboy168@gmail.com>
2022-06-29 10:44:51 +0800

    update

---
+++ bee/utility/file_handle_netbsd.cpp
@@ -0,0 +1,19 @@
+#include <bee/utility/file_handle.h>
+#include <bee/format.h>
+#include <fcntl.h>
+#include <sys/file.h>
+#include <unistd.h>
+
+namespace bee {
+    file_handle file_handle::lock(const fs::path& filename) {
+        int fd = ::open(filename.c_str(), O_WRONLY | O_CREAT | O_TRUNC, 0644);
+        if (fd == -1) {
+            return {};
+        }
+        if (::flock(fd, LOCK_EX | LOCK_NB) == -1) {
+            ::close(fd);
+            return {};
+        }
+        return {fd};
+    }
+}
