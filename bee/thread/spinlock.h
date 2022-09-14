00012cf161d8872927ba48d58d5dc95732b51682
pkubaj <pkubaj@FreeBSD.org>
2022-09-14 12:56:53 +0000

    Add support for powerpc*

--- bee/thread/spinlock.h
+++ bee/thread/spinlock.h
@@ -18,6 +18,8 @@
         asm volatile ("div %0, %0, zero" : "=r" (dummy));
         asm volatile ("" ::: "memory");
     }}
+#elif defined(__powerpc__)
+    namespace bee { inline void cpu_relax() { asm volatile("ori 0,0,0" ::: "memory"); }}
 #else
     #error unsupport platform
 #endif
