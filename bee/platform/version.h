1d577c46f89beeb4c28d208fe1bf775a7f54b029
actboy168 <actboy168@gmail.com>
2022-10-12 14:27:55 +0800

    整理代码

--- bee/platform/version.h
+++ bee/platform/version.h
@@ -10,17 +10,17 @@ namespace bee::platform {
 		uint32_t minor;
 		uint32_t revision;
 		uint32_t build;
-        bool operator<(const version& r) const {
-            const version& l = *this;
-            if (l.major < r.major) { return true; }
-            if (l.minor < r.minor) { return true; }
-            if (l.revision < r.revision) { return true; }
-            if (l.build < r.build) { return true; }
-            return false;
-        }
+		bool operator<(const version& r) const {
+			const version& l = *this;
+			if (l.major < r.major) { return true; }
+			if (l.minor < r.minor) { return true; }
+			if (l.revision < r.revision) { return true; }
+			if (l.build < r.build) { return true; }
+			return false;
+		}
 	};
 
-    version get_version();
+	version get_version();
 }
 
 #endif
