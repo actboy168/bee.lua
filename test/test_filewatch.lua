019c787a3d0b0216bdcd8b7e8d4c6dc91b3b256e
hasagi <543286489@qq.com>
2023-03-06 14:42:25 +0800

    增加指向自己的空软链和指向父目录的目录软链测试用例 (#36)
    Co-authored-by: jiantaoli <jiantaoli@tencent.com>
    Co-authored-by: actboy168 <actboy168@gmail.com>

--- test/test_filewatch.lua
+++ test/test_filewatch.lua
@@ -1,9 +1,22 @@
 local lt = require 'ltest'
 local filewatch = require 'bee.filewatch'
 local fs = require 'bee.filesystem'
+local platform = require 'bee.platform'
 local thread = require 'bee.thread'
 
 local test_fw = lt.test "filewatch"
+
+local isWindows = platform.os == 'windows'
+
+local function supportedSymlink()
+    if not isWindows then
+        return true
+    end
+    -- see https://blogs.windows.com/windowsdeveloper/2016/12/02/symlinks-windows-10/
+    local ok = pcall(fs.create_symlink, "temp.txt", "temp.link")
+    fs.remove_all "temp.link"
+    return ok
+end
 
 local function create_file(filename, content)
     fs.remove(filename)
@@ -19,7 +32,7 @@ local function test(f)
     fs.create_directories(root)
     local fw = filewatch.create()
     fw:set_recursive(true)
-    fw:set_follow_symlinks(false)
+    fw:set_follow_symlinks(true)
     fw:set_filter(function ()
         return true
     end)
@@ -71,4 +84,24 @@ function test_fw:test_2()
         assertHas(root / 'test1.txt')
         assertHas(root / 'test2.txt')
     end)
+end
+
+-- test unexist symlink link to self
+-- test directory symlink link to parent
+function test_fw:test_symlink()
+    if not supportedSymlink() then
+        return
+    end
+
+    local root = fs.absolute('./temp/'):lexically_normal()
+    pcall(fs.remove_all, root)
+    fs.create_directories(root)
+    fs.create_symlink(root / 'test1', root/'test1')
+    fs.create_symlink(root, root/'child')
+
+    local fw = filewatch.create()
+    fw:set_recursive(true)
+    fw:set_follow_symlinks(true)
+    fw:add(root:string())
+    pcall(fs.remove_all, root)
 end
