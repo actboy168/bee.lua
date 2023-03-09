5dedb3b4c46cf38011277688dd7dbb20b4a942f3
actboy168 <actboy168@gmail.com>
2023-03-09 11:45:42 +0800

    fixes bug

--- bee/nonstd/format.h
+++ bee/nonstd/format.h
@@ -2,7 +2,7 @@
 
 #if defined(__cpp_lib_print)
 #include <format>
-#elif defined(__cpp_lib_format)
+#elif defined(__cpp_lib_format) && (__cpp_lib_format >= 202200L) // see https://wg21.link/P2508R1
 #include <format>
 #include <bee/nonstd/3rd/fmt/format.h>
 #include <bee/nonstd/3rd/fmt/xchar.h>
