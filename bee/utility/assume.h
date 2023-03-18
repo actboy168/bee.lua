3514f24779e95c5e2e49e3d62d1543330165d613
actboy168 <actboy168@gmail.com>
2023-03-19 00:06:53 +0800

    update

--- bee/utility/assume.h
+++ bee/utility/assume.h
@@ -2,13 +2,6 @@
 
 #if defined(_MSC_VER)
 #    define ASSUME(cond) __assume(cond)
-#elif defined(__clang__)
-#    define ASSUME(cond) __builtin_assume(cond)
 #else
-#    define ASSUME(cond)             \
-        if (cond) {                  \
-        }                            \
-        else {                       \
-            __builtin_unreachable(); \
-        }
+#    define ASSUME(cond) ((cond) ? static_cast<void>(0) : __builtin_unreachable())
 #endif
