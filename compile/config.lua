fa13ce2fb61748f958f6a4f60c4bbe84316677bd
actboy168 <actboy168@gmail.com>
2023-03-24 16:28:48 +0800

    使用新的语法

--- compile/config.lua
+++ compile/config.lua
@@ -1,53 +1,49 @@
 local lm = require 'luamake'
+lm:required_version "1.2"
 
 lm.cxx = 'c++17'
 
-local plat = (function ()
-    if lm.os == 'windows' then
-        if lm.compiler == "gcc" then
-            return "mingw"
-        end
-        return "msvc"
-    end
-    return lm.os
-end)()
-lm.builddir = ("build/%s"):format(plat)
-
-lm.warnings = "error"
-lm.rtti = "off"
-
 if lm.sanitize then
-    lm.mode = "debug"
-    lm.flags = "-fsanitize=address"
-    lm.gcc = {
-        ldflags = "-fsanitize=address"
-    }
-    lm.clang = {
-        ldflags = "-fsanitize=address"
-    }
-    lm.msvc = {
-        defines = "_DISABLE_STRING_ANNOTATION",
-        flags = lm.mode == "release" and {
-            "/wd5072",
+    lm:config "sanitize" {
+        mode = "debug",
+        flags = "-fsanitize=address",
+        gcc = {
+            ldflags = "-fsanitize=address"
         },
-        ldflags = lm.mode == "release" and {
-            "/ignore:4302",
+        clang = {
+            ldflags = "-fsanitize=address"
+        },
+        msvc = {
+            defines = "_DISABLE_STRING_ANNOTATION",
+            flags = lm.mode == "release" and {
+                "/wd5072",
+            },
+            ldflags = lm.mode == "release" and {
+                "/ignore:4302",
+            }
         }
     }
 end
 
-lm.windows = {
-    defines = "_WIN32_WINNT=0x0601",
+lm:config "common" {
+    warnings = "error",
+    rtti = "off",
+    windows = {
+        defines = "_WIN32_WINNT=0x0601",
+    },
+    msvc = lm.mode == "debug" and lm.arch == "x86_64" and {
+        ldflags = "/STACK:" .. 0x160000
+    },
+    macos = {
+        flags = "-Wunguarded-availability",
+        sys = "macos10.15",
+    },
+    linux = {
+        flags = "-fPIC",
+    },
 }
-if lm.mode == "debug" and lm.arch == "x86_64" and lm.compiler == "msvc" then
-    lm.windows.ldflags = "/STACK:"..0x160000
-end
 
-lm.macos = {
-    flags = "-Wunguarded-availability",
-    sys = "macos10.15",
-}
-
-lm.linux = {
-    flags = "-fPIC",
+lm.configs = {
+    "common",
+    lm.sanitize and "sanitize"
 }
