2d7518b7d46816dc1de2a9f87b76d9a48f02e17c
actboy168 <actboy168@gmail.com>
2023-04-13 14:38:18 +0800

    帮gcc改bug

--- bee/nonstd/format.h
+++ bee/nonstd/format.h
@@ -4,6 +4,15 @@
 #    include <version>
 #endif
 
+#if defined(__GLIBCXX__)
+#    if __GLIBCXX__ >= 20230320 && __cplusplus >= 202002L
+#        if defined(__cpp_lib_format)
+#            undef __cpp_lib_format
+#        endif
+#        define __cpp_lib_format 202207L
+#    endif
+#endif
+
 #if defined(__cpp_lib_format) && (__cpp_lib_format >= 202200L)  // see https://wg21.link/P2508R1
 #    include <format>
 #else
