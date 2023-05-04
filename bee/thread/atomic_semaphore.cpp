e25d45cf04fceca0fb7bb8a255e219d367a5d49d
actboy168 <actboy168@gmail.com>
2023-05-04 17:24:26 +0800

    support macos/ios

--- bee/thread/atomic_semaphore.cpp
+++ bee/thread/atomic_semaphore.cpp
@@ -37,12 +37,23 @@ namespace bee {
     void kernel_wake(const T* ptr, bool all) {
         syscall(SYS_futex, ptr, FUTEX_WAKE_PRIVATE, all ? INT_MAX : 1, 0, 0, 0);
     }
+#elif defined(BEE_USE_ULOCK)
+    extern "C" int __ulock_wait(uint32_t operation, void* addr, uint64_t value, uint32_t timeout);
+    extern "C" int __ulock_wake(uint32_t operation, void* addr, uint64_t wake_value);
+    constexpr uint32_t UL_COMPARE_AND_WAIT = 1;
+    constexpr uint32_t ULF_WAKE_ALL        = 0x00000100;
+    template <typename T, std::enable_if_t<std::is_same_v<T, uint64_t>, int> = 0>
+    void kernel_wait(const T* ptr, T val) {
+        __ulock_wait(UL_COMPARE_AND_WAIT, const_cast<T*>(ptr), val, 0);
+    }
+    template <typename T, std::enable_if_t<std::is_same_v<T, uint64_t>, int> = 0>
+    void kernel_wake(const T* ptr, bool all) {
+        __ulock_wake(UL_COMPARE_AND_WAIT | (all ? ULF_WAKE_ALL : 0), const_cast<T*>(ptr), 0);
+    }
 #else
-    // TODO ulock_wait/umtx_op
+    // TODO *bsd
 #    define BEE_NO_KERNEL_WAIT
 #endif
-    constexpr uint32_t SEM_FALSE = UINT32_C(0);
-    constexpr uint32_t SEM_TRUE  = UINT32_C(1);
 
     atomic_semaphore::atomic_semaphore() noexcept
         : v(SEM_FALSE) {
@@ -52,7 +63,7 @@ namespace bee {
 #if !defined(BEE_NO_KERNEL_WAIT)
         const auto* ptr = &v;
         v.store(SEM_TRUE);
-        kernel_wake((const uint32_t*)ptr, false);
+        kernel_wake((const value_type*)ptr, false);
 #else
         v.store(SEM_TRUE);
 #endif
@@ -63,7 +74,7 @@ namespace bee {
         for (;;) {
             if (v.exchange(SEM_FALSE) == SEM_TRUE)
                 return;
-            kernel_wait((const uint32_t*)&v, SEM_FALSE);
+            kernel_wait((const value_type*)&v, SEM_FALSE);
         }
 #else
         for (int i = 0; i < 64; ++i) {
