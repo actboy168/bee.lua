1f01891c5dfcb0a740e2b573eeefd4ea4efd9a37
actboy168 <actboy168@gmail.com>
2024-03-13 13:20:27 +0800

    兼容xcode15.3

--- bee/nonstd/charconv.h
+++ bee/nonstd/charconv.h
@@ -4,6 +4,18 @@
 #    include <version>
 #endif
 
+#if !defined(__cpp_lib_to_chars) && (__cplusplus >= 202000)
+#    if defined(__ENVIRONMENT_MAC_OS_X_VERSION_MIN_REQUIRED__)
+#        if defined(__MAC_14_4) && (__ENVIRONMENT_MAC_OS_X_VERSION_MIN_REQUIRED__ >= 130300)
+#            define __cpp_lib_to_chars 201611L
+#        endif
+#    elif defined(__ENVIRONMENT_IPHONE_OS_VERSION_MIN_REQUIRED__)
+#        if defined(__IPHONE_17_4) && (__ENVIRONMENT_IPHONE_OS_VERSION_MIN_REQUIRED__ >= 160300)
+#            define __cpp_lib_to_chars 201611L
+#        endif
+#    endif
+#endif
+
 #if defined(__cpp_lib_to_chars)
 #    include <charconv>
 #else
