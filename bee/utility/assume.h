9f03beba5402f448ba50fca276c86dcb8d124153
actboy168 <actboy168@gmail.com>
2024-04-12 20:55:37 +0800

    支持标准的assume

--- bee/utility/assume.h
+++ bee/utility/assume.h
@@ -1,7 +1,11 @@
 #pragma once
 
-#if defined(_MSC_VER)
-#    define BEE_ASSUME(cond) __assume(cond)
+#if defined(__has_cpp_attribute) && __has_cpp_attribute(assume) >= 202207
+#    define BEE_ASSUME(cond) [[assume(cond)]]
 #else
-#    define BEE_ASSUME(cond) ((cond) ? (void)(0) : __builtin_unreachable())
+#    if defined(_MSC_VER)
+#        define BEE_ASSUME(cond) __assume(cond)
+#    else
+#        define BEE_ASSUME(cond) ((cond) ? (void)(0) : __builtin_unreachable())
+#    endif
 #endif
