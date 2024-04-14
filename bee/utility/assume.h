7b65ba9a9375c5b16babe92e05bcb06a9d9378cd
actboy168 <actboy168@gmail.com>
2024-04-14 22:44:58 +0800

    fixes bug

--- bee/utility/assume.h
+++ bee/utility/assume.h
@@ -1,8 +1,12 @@
 #pragma once
 
-#if defined(__has_cpp_attribute) && __has_cpp_attribute(assume) >= 202207
-#    define BEE_ASSUME(cond) [[assume(cond)]]
-#else
+#if defined(__has_cpp_attribute)
+#    if __has_cpp_attribute(assume) >= 202207
+#        define BEE_ASSUME(cond) [[assume(cond)]]
+#    endif
+#endif
+
+#if !defined(BEE_ASSUME)
 #    if defined(_MSC_VER)
 #        define BEE_ASSUME(cond) __assume(cond)
 #    else
