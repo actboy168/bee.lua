1c1b7f17e937700ce79f87469f88785a174b1ee3
actboy168 <actboy168@gmail.com>
2024-04-22 17:31:08 +0800

    cleanup

--- binding/lua_epoll.cpp
+++ binding/lua_epoll.cpp
@@ -1,7 +1,9 @@
 #include <bee/error.h>
+#include <bee/lua/binding.h>
 #include <bee/lua/error.h>
 #include <bee/net/bpoll.h>
 #include <bee/nonstd/to_underlying.h>
+#include <bee/utility/dynarray.h>
 #include <binding/binding.h>
 #include <binding/luaref.h>
 
@@ -10,47 +12,46 @@ namespace bee::lua_epoll {
 
     struct lua_epoll {
         net::bpoll_handle fd;
-        size_t max_events;
-        int i;
-        int n;
+        int i = 0;
+        int n = 0;
         luaref ref;
-        net::bpoll_event_t events[1];
+        dynarray<net::bpoll_event_t> events;
+        lua_epoll(lua_State *L, net::bpoll_handle epfd, size_t max_events)
+            : fd(epfd)
+            , ref(luaref_init(L))
+            , events(max_events) {
+        }
     };
-
-    struct lua_epoll *ep_get(lua_State *L) {
-        return (struct lua_epoll *)luaL_checkudata(L, 1, "bee::epoll");
-    }
-
     static net::bpoll_socket ep_tofd(lua_State *L, int idx) {
         return (net::bpoll_socket)(intptr_t)lua_touserdata(L, idx);
     }
 
     static int ep_events(lua_State *L) {
-        struct lua_epoll *ep = (struct lua_epoll *)lua_touserdata(L, lua_upvalueindex(1));
-        if (ep->i >= ep->n) {
+        auto& ep = *(lua_epoll*)lua_touserdata(L, lua_upvalueindex(1));
+        if (ep.i >= ep.n) {
             return 0;
         }
-        const net::bpoll_event_t &ev = ep->events[ep->i];
-        luaref_get(ep->ref, L, ev.data.u32);
+        const net::bpoll_event_t &ev = ep.events[ep.i];
+        luaref_get(ep.ref, L, ev.data.u32);
         lua_pushinteger(L, static_cast<uint32_t>(ev.events));
-        ep->i++;
+        ep.i++;
         return 2;
     }
 
     static int ep_wait(lua_State *L) {
-        struct lua_epoll *ep = ep_get(L);
+        auto& ep = lua::checkudata<lua_epoll>(L, 1);
         int timeout          = (int)luaL_optinteger(L, 2, -1);
-        int n                = net::bpoll_wait(ep->fd, { ep->events, ep->max_events }, timeout);
+        int n                = net::bpoll_wait(ep.fd, ep.events, timeout);
         if (n == -1) {
             return lua::push_error(L, error::net_errmsg("epoll_wait"));
         }
-        ep->i = 0;
-        ep->n = n;
+        ep.i = 0;
+        ep.n = n;
         lua_getiuservalue(L, 1, 2);
         return 1;
     }
 
-    static bool ep_close_epoll(struct lua_epoll *ep) {
+    static bool ep_close_epoll(lua_epoll& ep) {
         if (!net::bpoll_close(ep->fd)) {
             return false;
         }
@@ -59,7 +60,7 @@ namespace bee::lua_epoll {
     }
 
     static int ep_close(lua_State *L) {
-        struct lua_epoll *ep = ep_get(L);
+        auto &ep = lua::checkudata<lua_epoll>(L, 1);
         if (!ep_close_epoll(ep)) {
             return lua::push_error(L, error::net_errmsg("epoll_close"));
         }
@@ -68,14 +69,14 @@ namespace bee::lua_epoll {
     }
 
     static int ep_mt_gc(lua_State *L) {
-        struct lua_epoll *ep = ep_get(L);
+        auto &ep = lua::checkudata<lua_epoll>(L, 1);
         ep_close_epoll(ep);
-        luaref_close(ep->ref);
+        luaref_close(ep.ref);
         return 0;
     }
 
     static int ep_mt_close(lua_State *L) {
-        struct lua_epoll *ep = ep_get(L);
+        auto& ep = lua::checkudata<lua_epoll>(L, 1);
         ep_close_epoll(ep);
         return 0;
     }
@@ -117,8 +118,8 @@ namespace bee::lua_epoll {
     }
 
     static int ep_event_add(lua_State *L) {
-        struct lua_epoll *ep = ep_get(L);
-        if (ep->fd == bpoll_invalid_handle) {
+        auto& ep = lua::checkudata<lua_epoll>(L, 1);
+        if (ep.fd == bpoll_invalid_handle) {
             return lua::push_error(L, error::crt_errmsg("epoll_ctl", std::errc::bad_file_descriptor));
         }
         net::bpoll_socket fd = ep_tofd(L, 2);
@@ -128,15 +129,15 @@ namespace bee::lua_epoll {
         else {
             lua_pushvalue(L, 4);
         }
-        int r = luaref_ref(ep->ref, L);
+        int r = luaref_ref(ep.ref, L);
         if (r == LUA_NOREF) {
             return lua::push_error(L, "Too many events.");
         }
         net::bpoll_event_t ev;
         ev.events   = static_cast<decltype(ev.events)>(luaL_checkinteger(L, 3));
         ev.data.u32 = r;
-        if (!net::bpoll_ctl_add(ep->fd, fd, ev)) {
-            luaref_unref(ep->ref, r);
+        if (!net::bpoll_ctl_add(ep.fd, fd, ev)) {
+            luaref_unref(ep.ref, r);
             return lua::push_error(L, error::net_errmsg("epoll_ctl"));
         }
         storeref(L, r);
@@ -145,7 +146,7 @@ namespace bee::lua_epoll {
     }
 
     static int ep_event_mod(lua_State *L) {
-        struct lua_epoll *ep = ep_get(L);
+        auto& ep = lua::checkudata<lua_epoll>(L, 1);
         net::bpoll_socket fd = ep_tofd(L, 2);
         int r                = findref(L);
         if (r == LUA_NOREF) {
@@ -154,7 +155,7 @@ namespace bee::lua_epoll {
         net::bpoll_event_t ev;
         ev.events   = static_cast<decltype(ev.events)>(luaL_checkinteger(L, 3));
         ev.data.u32 = r;
-        if (!net::bpoll_ctl_mod(ep->fd, fd, ev)) {
+        if (!net::bpoll_ctl_mod(ep.fd, fd, ev)) {
             return lua::push_error(L, error::net_errmsg("epoll_ctl"));
         }
         lua_pushboolean(L, 1);
@@ -162,17 +163,37 @@ namespace bee::lua_epoll {
     }
 
     static int ep_event_del(lua_State *L) {
-        struct lua_epoll *ep = ep_get(L);
+        auto& ep = lua::checkudata<lua_epoll>(L, 1);
         net::bpoll_socket fd = ep_tofd(L, 2);
-        if (!net::bpoll_ctl_del(ep->fd, fd)) {
+        if (!net::bpoll_ctl_del(ep.fd, fd)) {
             return lua::push_error(L, error::net_errmsg("epoll_ctl"));
         }
         int r = cleanref(L);
         if (r != LUA_NOREF) {
-            luaref_unref(ep->ref, r);
+            luaref_unref(ep.ref, r);
         }
         lua_pushboolean(L, 1);
         return 1;
+    }
+
+    static void metatable(lua_State *L) {
+        static luaL_Reg lib[] = {
+            { "wait", ep_wait },
+            { "close", ep_close },
+            { "event_add", ep_event_add },
+            { "event_mod", ep_event_mod },
+            { "event_del", ep_event_del },
+            { NULL, NULL }
+        };
+        luaL_newlibtable(L, lib);
+        luaL_setfuncs(L, lib, 0);
+        lua_setfield(L, -2, "__index");
+        static luaL_Reg mt[] = {
+            { "__gc", ep_mt_gc },
+            { "__close", ep_mt_close },
+            { NULL, NULL }
+        };
+        luaL_setfuncs(L, mt, 0);
     }
 
     static int ep_create(lua_State *L) {
@@ -184,35 +205,12 @@ namespace bee::lua_epoll {
         if (epfd == bpoll_invalid_handle) {
             return lua::push_error(L, error::net_errmsg("epoll_create"));
         }
-        size_t sz            = sizeof(struct lua_epoll) + (size_t)(max_events - 1) * sizeof(net::bpoll_event_t);
-        struct lua_epoll *ep = (struct lua_epoll *)lua_newuserdatauv(L, sz, 2);
+        lua::newudata<lua_epoll>(L, L, epfd, max_events);
         lua_newtable(L);
         lua_setiuservalue(L, -2, 1);
         lua_pushvalue(L, -1);
         lua_pushcclosure(L, ep_events, 1);
         lua_setiuservalue(L, -2, 2);
-        ep->fd         = epfd;
-        ep->max_events = max_events;
-        ep->ref        = luaref_init(L);
-        ep->i          = 0;
-        ep->n          = 0;
-        if (luaL_newmetatable(L, "bee::epoll")) {
-            luaL_Reg l[] = {
-                { "wait", ep_wait },
-                { "close", ep_close },
-                { "event_add", ep_event_add },
-                { "event_mod", ep_event_mod },
-                { "event_del", ep_event_del },
-                { "__gc", ep_mt_gc },
-                { "__close", ep_mt_close },
-                { "__index", NULL },
-                { NULL, NULL },
-            };
-            luaL_setfuncs(L, l, 0);
-            lua_pushvalue(L, -1);
-            lua_setfield(L, -2, "__index");
-        }
-        lua_setmetatable(L, -2);
         return 1;
     }
 
@@ -245,3 +243,12 @@ namespace bee::lua_epoll {
 }
 
 DEFINE_LUAOPEN(epoll)
+
+namespace bee::lua {
+    template <>
+    struct udata<lua_epoll::lua_epoll> {
+        static inline int nupvalue   = 2;
+        static inline auto name      = "bee::lua_epoll";
+        static inline auto metatable = bee::lua_epoll::metatable;
+    };
+}
