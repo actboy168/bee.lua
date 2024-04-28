4f8da4911d95433159926f98da95c5e74fa33997
actboy168 <actboy168@gmail.com>
2024-04-28 21:12:24 +0800

    这个测试可能太严格了

--- test/test_skip.lua
+++ test/test_skip.lua
@@ -1,15 +1,23 @@
 local platform = require "bee.platform"
 local lt = require "ltest"
 
+local SKIP <const> = lt.skip
+
+if platform.os == "macos" then
+    SKIP "thread.test_sleep"
+end
+
 if platform.os == "freebsd" then
-    lt.skip "filewatch"
-    lt.skip "filesystem.test_symlink"
+    SKIP "filewatch"
+    SKIP "filesystem.test_symlink"
 end
 
 if platform.os == "openbsd" then
-    lt.skip "filewatch"
+    SKIP "filewatch"
+    SKIP "thread.test_sleep"
 end
 
 if platform.os == "netbsd" then
-    lt.skip "filewatch"
+    SKIP "filewatch"
+    SKIP "thread.test_sleep"
 end
