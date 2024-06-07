72e2f38226595f3c6b1a5cf30e2bb29f8cb57c0b
Hanchin Hsieh <me@yuchanns.xyz>
2024-06-07 15:57:23 +0800

    fix(unwind): convert ucontext_t to unw_context_t on linux aarch64 (#46)

--- bee/crash/unwind_linux.cpp
+++ bee/crash/unwind_linux.cpp
@@ -6,7 +6,12 @@
 namespace bee::crash {
     void unwind(ucontext_t *ctx, uint16_t skip, unwind_callback func, void *ud) noexcept {
         unw_cursor_t cursor;
+#if defined(__aarch64__)
+        unw_context_t *unw_ctx = (unw_context_t *)ctx;
+        unw_init_local(&cursor, unw_ctx);
+#else
         unw_init_local(&cursor, ctx);
+#endif
         while (unw_step(&cursor) > 0) {
             unw_word_t pc;
             unw_get_reg(&cursor, UNW_REG_IP, &pc);
