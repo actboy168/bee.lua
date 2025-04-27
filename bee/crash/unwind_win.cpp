e38112525899f55d5496ee8ec0d7d3a9650dbafa
actboy168 <actboy168@gmail.com>
2025-04-27 10:50:22 +0800

    修复arm的编译问题

--- bee/crash/unwind_win.cpp
+++ bee/crash/unwind_win.cpp
@@ -9,7 +9,12 @@
 
 namespace bee::crash {
     void unwind(const CONTEXT* ctx, uint16_t skip, unwind_callback func, void* ud) noexcept {
-#if defined(_M_X64)
+#if defined(_M_X64) || defined(_M_ARM64)
+#    if defined(_M_X64)
+#        define CONTEXT_REG_PC Rip
+#    elif defined(_M_ARM64)
+#        define CONTEXT_REG_PC Pc
+#    endif
         CONTEXT Context;
         UNWIND_HISTORY_TABLE UnwindHistoryTable;
         PRUNTIME_FUNCTION RuntimeFunction;
@@ -19,21 +24,26 @@ namespace bee::crash {
         memcpy(&Context, ctx, sizeof(CONTEXT));
         RtlZeroMemory(&UnwindHistoryTable, sizeof(UNWIND_HISTORY_TABLE));
         for (;;) {
-            RuntimeFunction = RtlLookupFunctionEntry(Context.Rip, &ImageBase, &UnwindHistoryTable);
+            RuntimeFunction = RtlLookupFunctionEntry(Context.CONTEXT_REG_PC, &ImageBase, &UnwindHistoryTable);
             if (!RuntimeFunction) {
+#    if defined(_M_X64)
                 Context.Rip = (ULONG64)(*(PULONG64)Context.Rsp);
-                Context.Rsp += 8;
+                Context.Rsp += sizeof(ULONG64);
+#    elif defined(_M_ARM64)
+                Context.Pc = Context.Lr;
+#    endif
+                break;
             } else {
-                RtlVirtualUnwind(UNW_FLAG_NHANDLER, ImageBase, Context.Rip, RuntimeFunction, &Context, &HandlerData, &EstablisherFrame, NULL);
+                RtlVirtualUnwind(UNW_FLAG_NHANDLER, ImageBase, Context.CONTEXT_REG_PC, RuntimeFunction, &Context, &HandlerData, &EstablisherFrame, NULL);
             }
-            if (!Context.Rip) {
+            if (!Context.CONTEXT_REG_PC) {
                 break;
             }
             if (skip > 0) {
                 skip--;
                 continue;
             }
-            if (!func((void*)Context.Rip, ud)) {
+            if (!func((void*)Context.CONTEXT_REG_PC, ud)) {
                 break;
             }
         }
