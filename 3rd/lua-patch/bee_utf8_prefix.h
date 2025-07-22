da33fccd374b5b53f895574240e35a73e81f4ab2
Bai Miao <68489543+ChrisCatCP@users.noreply.github.com>
2025-07-22 23:56:38 +0800

    修复使用onelua编译无法引用utf8_loadlibrary

--- 3rd/lua-patch/bee_utf8_prefix.h
+++ 3rd/lua-patch/bee_utf8_prefix.h
@@ -37,12 +37,10 @@
 #    define getenv(...) utf8_getenv(__VA_ARGS__)
 #    define tmpnam(...) utf8_tmpnam(__VA_ARGS__)
 
-#    if defined(loadlib_c)
-#        include <Windows.h>
-#        define LoadLibraryExA(...) utf8_LoadLibraryExA(__VA_ARGS__)
-#        define GetModuleFileNameA(...) utf8_GetModuleFileNameA(__VA_ARGS__)
-#        define FormatMessageA(...) utf8_FormatMessageA(__VA_ARGS__)
-#    endif
+#    include <Windows.h>
+#    define LoadLibraryExA(...) utf8_LoadLibraryExA(__VA_ARGS__)
+#    define GetModuleFileNameA(...) utf8_GetModuleFileNameA(__VA_ARGS__)
+#    define FormatMessageA(...) utf8_FormatMessageA(__VA_ARGS__)
 #endif
 
 #define lua_writestring(s, l) utf8_ConsoleWrite(s, (int)l)
