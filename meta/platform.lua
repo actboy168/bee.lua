3206446672a5f08e2942b93c3f4d9c1a033573db
最萌小汐 <sumneko@hotmail.com>
2025-12-05 22:20:41 +0800

    修复 platform 的meta文件错误

--- meta/platform.lua
+++ meta/platform.lua
@@ -2,14 +2,15 @@
 
 ---平台信息（模块返回的是表而不是类）
 ---@class bee.platform
----@field os string 操作系统名称: "windows"|"android"|"linux"|"netbsd"|"freebsd"|"openbsd"|"ios"|"macos"|"unknown"
----@field Compiler string 编译器类型: "clang"|"msvc"|"gcc"|"unknown"
+---@field os "windows"|"android"|"linux"|"netbsd"|"freebsd"|"openbsd"|"ios"|"macos"|"unknown" 操作系统名称
+---@field Compiler "clang"|"msvc"|"gcc"|"unknown" 编译器类型
 ---@field CompilerVersion string 编译器版本字符串
----@field CRT string C运行时库类型: "bionic"|"msvc"|"libstdc++"|"libc++"|"unknown"
+---@field CRT "bionic"|"msvc"|"libstdc++"|"libc++"|"unknown" C运行时库类型
 ---@field CRTVersion string C运行时库版本字符串
----@field Arch string 架构: "arm64"|"x86"|"x86_64"|"arm"|"riscv"|"wasm32"|"wasm64"|"mips64el"|"loongarch64"|"ppc64"|"ppc"|"unknown"
+---@field Arch "arm64"|"x86"|"x86_64"|"arm"|"riscv"|"wasm32"|"wasm64"|"mips64el"|"loongarch64"|"ppc64"|"ppc"|"unknown" 架构
 ---@field DEBUG boolean 是否为调试构建
 ---@field os_version bee.platform.os_version 操作系统版本信息
+local platform = {}
 
 ---操作系统版本信息
 ---@class bee.platform.os_version
@@ -17,4 +18,4 @@
 ---@field minor integer 次版本号
 ---@field revision integer 修订版本号
 
-return {}
+return platform
